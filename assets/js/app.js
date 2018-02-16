38f8fc738cca2dc2993c8518402c27926896837a
keslymartinez <keslymartinezvaldes@gmail.com>
2018-02-16 16:22:36 -0300

    subiendo login

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,19 @@
-
+            
 
 
     
     // Setup an event listener to make an API call once auth is complete
     function onLinkedInLoad() {
         IN.Event.on(IN, "auth", getProfileData);
+        IN.User.authorize(callbackFunction, callbackScope);
+        IN.API.Raw( https:'//api.linkedin.com/v1/people/~?format=json' ).method(methodType).body("<p id=\""+namber.id+"\">hello"+namber.firstName+"</p>").result(resultCallback);
     }
 
+
+   /* function onLinkedInLoad(){
+        IN.API.Profile("me"),result(displayProfiles);
+    }
+    */
     // Handle the successful return from the API call
     function onSuccess(data) {
         console.log(data);
@@ -21,3 +28,11 @@
     function getProfileData() {
         IN.API.Raw("/people/~").result(onSuccess).error(onError);
     }
+
+
+/* function displayProfiles(profiles){
+        namber = profiles.values[0];
+        document.getElementById("profiles").innerHTML= "<p id=\""+namber.id+"\">hello"+namber.firstName+"</p>";
+
+    }
+    */
