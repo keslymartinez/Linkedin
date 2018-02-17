a4809d3ae2a40e82b2d72a4b028af5c5ff04b36c
NadiaMorales <nadia.morales.leiva@gmail.com>
2018-02-17 17:48:20 -0300

    inicio de sesion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,38 @@
+function onLinkedInLoad() {
+    IN.Event.on(IN, "auth", function () { onLinkedInLogin(); });
+    IN.Event.on(IN, "logout", function () { onLinkedInLogout(); });
+}
 
+function onLinkedInLogout() {
+    setConnections({}, { total: 0 });
+}
 
+function onLinkedInLogin() {
+    IN.API.Connections("me")
+        .fields("id", "firstName", "lastName", "pictureUrl", "publicProfileUrl")
+        .result(function (result, metadata) {
+            setConnections(result.values, metadata);
+        });
+}
 
-    
-    // Setup an event listener to make an API call once auth is complete
-    function onLinkedInLoad() {
-        IN.Event.on(IN, "auth", getProfileData);
+function setConnections(connections) {
+    console.log('dos')
+    var connHTML = "<ul>";
+    for (id in connections) {
+        connHTML = connHTML + "<li><a href=\"" + connections[id].publicProfileUrl + "\">";
+
+        if (connections[id].hasOwnProperty('pictureUrl')) {
+            connHTML = connHTML + "<img align=\"baseline\" src=\"" + connections[id].pictureUrl + "\"></a>";
+        } else {
+            connHTML = connHTML + "<img align=\"baseline\" \n\
+        src=\"http://linkedin.com/scds/common/u/img/icon/icon_no_photo_80x80.png\"></a>";
+        }
+
+        connHTML = connHTML + "&nbsp;<a href=\"" + connections[id].publicProfileUrl + "\">";
+        connHTML = connHTML + connections[id].firstName + " " + connections[id].lastName + "</a>";
+        connHTML = connHTML + " (memberToken: " + connections[id].id + ")</li>";
     }
 
-    // Handle the successful return from the API call
-    function onSuccess(data) {
-        console.log(data);
-    }
-
-    // Handle an error response from the API call
-    function onError(error) {
-        console.log(error);
-    }
-
-    // Use the API call wrapper to request the member's basic profile data
-    function getProfileData() {
-        IN.API.Raw("/people/~").result(onSuccess).error(onError);
-    }
+    connHTML = connHTML + "</ul>";
+    document.getElementById("connectionsdata").innerHTML = connHTML;
+}
